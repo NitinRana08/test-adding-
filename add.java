141127195a8a8c10013950e20ebde20478fb53dc
NitinRana08 <nitinr8229@gmail.com>
2024-10-04 13:10:18 +0530

    Initial commit

---
+++ add.java
@@ -0,0 +1,9 @@
+public class add {
+    public static void main(String[] args) {
+        int a=5;
+        int b=6;
+        int ans=a+b;
+        System.out.println("ans is : "+ans);
+    }
+    
+}
